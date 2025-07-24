adc358508e6256c8ca84313ec0774b3087a800d3
ketoni <juhana.kekoni@csc.fi>
2025-07-24 14:55:51 +0300

    WIP: Tab content

--- src/components/DatasetSelect.vue
+++ src/components/DatasetSelect.vue
@@ -2,7 +2,7 @@
 import { ref, computed, watchEffect } from 'vue'
 import { useDatasets } from '@/composables/datasets'
 
-const { datasets, setCurrent, clearCurrent } = useDatasets()
+const { datasets, setCurrent, clearCurrent, currentDataset } = useDatasets()
 
 // Dropdown selections
 const selectedProducer = ref<string>('')
@@ -111,18 +111,11 @@ const tab = ref<Tab>('tab1')
 </script>
 
 <template>
-  <div class="dropdowns">
+  <div class="controls">
     <label>
       Producer:
-      <select
-        v-model="selectedProducer"
-        :disabled="producerOptions.length <= 1"
-      >
-        <option
-          v-for="producer in producerOptions"
-          :key="producer"
-          :value="producer"
-        >
+      <select v-model="selectedProducer" :disabled="producerOptions.length <= 1">
+        <option v-for="producer in producerOptions" :key="producer" :value="producer">
           {{ producer }}
         </option>
       </select>
@@ -137,78 +130,87 @@ const tab = ref<Tab>('tab1')
       </select>
     </label>
 
-    <c-tabs v-model="tab" v-control>
-      <c-tab value="tab1">Info</c-tab>
-      <c-tab value="tab2">Links</c-tab>
-      <c-tab value="tab3">Settings</c-tab>
+    <div v-if="!currentDataset" class="suggestion">
+      <p>Please select a Producer to start browsing available datasets.</p>
+    </div>
+    <div v-else class="extra">
+      <c-tabs v-model="tab" v-control>
+        <c-tab value="tab1">Info</c-tab>
+        <c-tab value="tab2">Links</c-tab>
+        <c-tab value="tab3">Settings</c-tab>
 
-      <!-- eslint-disable-next-line vue/no-deprecated-slot-attribute -->
-      <c-tab-items slot="items">
-        <c-tab-item value="tab1">
-          This is the content for the first tab
-        </c-tab-item>
+        <!-- eslint-disable-next-line vue/no-deprecated-slot-attribute -->
+        <c-tab-items slot="items">
+          <c-button ghost class="read-more">Read more</c-button>
+          <c-tab-item value="tab1" class="faded">
+            This is the content for the first tab<br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, harum pariatur deserunt obcaecati maiores cupiditate magnam! Minima omnis libero sequi consectetur nesciunt. Facilis maxime ratione molestiae expedita quis porro id?<br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, harum pariatur deserunt obcaecati maiores cupiditate magnam! Minima omnis libero sequi consectetur nesciunt. Facilis maxime ratione molestiae expedita quis porro id?<br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, harum pariatur deserunt obcaecati maiores cupiditate magnam! Minima omnis libero sequi consectetur nesciunt. Facilis maxime ratione molestiae expedita quis porro id?<br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, harum pariatur deserunt obcaecati maiores cupiditate magnam! Minima omnis libero sequi consectetur nesciunt. Facilis maxime ratione molestiae expedita quis porro id?<br>
+          </c-tab-item>
 
-        <c-tab-item value="tab2">
-          This is the content for the second tab
-        </c-tab-item>
+          <c-tab-item value="tab2">
+            This is the content for the second tab<br>
+            Some links<br>
+            Some links<br>
+            Some links<br>
+            Some links<br>
+            Some links<br>
+            Some links<br>
+            Some links<br>
+            Some links<br>
+            Some links<br>
+            Some links<br>
+            Some links<br>
+            Some links<br>
+          </c-tab-item>
 
-        <c-tab-item value="tab3">
-          <label>
-            Scale:
-            <select
-              v-model="selectedScale"
-              :disabled="scaleOptions.length <= 1"
-            >
-              <option v-for="scale in scaleOptions" :key="scale" :value="scale">
-                {{ scale }}
-              </option>
-            </select>
-          </label>
+          <c-tab-item value="tab3">
+            <label>
+              Scale:
+              <select v-model="selectedScale" :disabled="scaleOptions.length <= 1" >
+                <option v-for="scale in scaleOptions" :key="scale" :value="scale" >
+                  {{ scale }}
+                </option>
+              </select>
+            </label>
 
-          <label>
-            Year:
-            <select v-model="selectedYear" :disabled="yearOptions.length <= 1">
-              <option v-for="year in yearOptions" :key="year" :value="year">
-                {{ year }}
-              </option>
-            </select>
-          </label>
+            <label>
+              Year:
+              <select v-model="selectedYear" :disabled="yearOptions.length <= 1" >
+                <option v-for="year in yearOptions" :key="year" :value="year">
+                  {{ year }}
+                </option>
+              </select>
+            </label>
 
-          <label>
-            Format:
-            <select
-              v-model="selectedFormat"
-              :disabled="formatOptions.length <= 1"
-            >
-              <option
-                v-for="format in formatOptions"
-                :key="format"
-                :value="format"
-              >
-                {{ format }}
-              </option>
-            </select>
-          </label>
+            <label>
+              Format:
+              <select v-model="selectedFormat" :disabled="formatOptions.length <= 1" >
+                <option v-for="format in formatOptions" :key="format" :value="format" >
+                  {{ format }}
+                </option>
+              </select>
+            </label>
 
-          <label>
-            Coordinate System:
-            <select
-              v-model="selectedCoordSys"
-              :disabled="coordSysOptions.length <= 1"
-            >
-              <option v-for="cs in coordSysOptions" :key="cs" :value="cs">
-                {{ cs }}
-              </option>
-            </select>
-          </label>
-        </c-tab-item>
-      </c-tab-items>
-    </c-tabs>
+            <label>
+              Coordinate System:
+              <select v-model="selectedCoordSys" :disabled="coordSysOptions.length <= 1" >
+                <option v-for="cs in coordSysOptions" :key="cs" :value="cs">
+                  {{ cs }}
+                </option>
+              </select>
+            </label>
+          </c-tab-item>
+        </c-tab-items>
+      </c-tabs>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.dropdowns {
+.controls {
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
@@ -222,5 +224,25 @@ c-tabs {
   --c-tab-background-color-hover: var(--c-primary-400);
   --c-tab-text-color: white;
   --c-tabs-indicator-color: var(--c-primary-100);
+}
+c-tab-item {
+  height: 250px;
+}
+.faded {
+  mask-image: linear-gradient(to bottom, black 50%, transparent 100%);
+  -webkit-mask-image: linear-gradient(to bottom, black 50%, transparent 100%);
+}
+.suggestion {
+  margin: 5px;
+  color: yellow;
+}
+
+.read-more {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  background: var(--c-primary-500);
+  color: white;
+  z-index: 1;
 }
 </style>
