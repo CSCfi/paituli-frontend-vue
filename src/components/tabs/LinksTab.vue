d3ab1d4d4bff76a5e6169d8f95d1521838672e59
ketoni <juhana.kekoni@csc.fi>
2025-08-11 15:54:44 +0300

    Tweaks on the Links tab

--- src/components/tabs/LinksTab.vue
+++ src/components/tabs/LinksTab.vue
@@ -4,22 +4,25 @@ import { useDatasets } from '@/composables/datasets'
 import { mdiOpenInNew } from '@mdi/js';
 
 const { currentDataset } = useDatasets()
-
 </script>
 
 <template>
-  <div v-if="currentDataset?.meta" class="metadata-info-label">
+  <div v-if="currentDataset?.meta">
     <p>
       You can view more metadata on
       <c-link :href="URLS.ETSIN_METADATA_BASE + currentDataset.meta" target="_blank">
         Etsin<c-icon :path="mdiOpenInNew" color="var(--c-primary-100)" size="18" />
       </c-link>
-      , and view or download dataset contents using
+    </p>
+  </div>
+  <div v-if="currentDataset?.funet">
+    <p>
+      You can view or download dataset contents using
       <c-link :href="URLS.HTTP_LINKS_BASE+ currentDataset.funet" target="_blank">
         HTTP<c-icon :path="mdiOpenInNew" color="var(--c-primary-100)" size="18" />
       </c-link>
       , FTP or rsync.
-      <c-link :href="URLS.HTTP_LINKS_BASE+ currentDataset.funet" target="_blank">
+      <c-link href="/files" target="_blank">
         Help?<c-icon :path="mdiOpenInNew" color="var(--c-primary-100)" size="18" />
       </c-link>
     </p>
@@ -31,10 +34,12 @@ const { currentDataset } = useDatasets()
       :code="'rsync://rsync.nic.funet.fi/ftp/index/geodata/' + currentDataset.funet"
       highlightjs
     />
-    Names, paths and geometry as
-    <c-link :href="URLS.HTTP_LINKS_BASE+ currentDataset.funet" target="_blank">
-      Shapefile<c-icon :path="mdiOpenInNew" color="var(--c-primary-100)" size="18" />
-    </c-link>
+    <p>
+      Names, paths and geometry as
+      <c-link :href="URLS.HTTP_LINKS_BASE+ currentDataset.funet" target="_blank">
+        Shapefile<c-icon :path="mdiOpenInNew" color="var(--c-primary-100)" size="18" />
+      </c-link>
+    </p>
   </div>
 
 
@@ -43,13 +48,6 @@ const { currentDataset } = useDatasets()
 <style scoped>
 c-link {
   --c-link-color: white;
-}
-.metadata-info-label {
-  margin-bottom: 10px;
-}
-.error {
-  color: red;
-  font-style: italic;
 }
 a {
   color: var(--c-info-400);
