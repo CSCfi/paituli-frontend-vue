699a262c4959ad9283e107a52d91ee740ac21635
ketoni <juhana.kekoni@csc.fi>
2025-09-20 23:38:13 +0300

    ServicesTab: Adjust buttons layout

--- src/components/tabs/ServicesTab.vue
+++ src/components/tabs/ServicesTab.vue
@@ -11,24 +11,21 @@ const servicesModal = ref()
   <p>
     Paituli provides various alternative web services and APIs for accessing and downloading its datasets.
   </p>
-  <p>
-    For detailed documentation on how to use these services,
-    please refer to the
-    <c-link href='/webservices' target="_blank">
-      documentation<c-icon :path="mdiOpenInNew" size="18" />
-    </c-link>
-  </p>
-  <div>
+  <div class="buttons">
     <c-button @click="servicesModal.open('FileTransferTab')">
       File transfer
     </c-button>
-  </div>
-
-  <div>
     <c-button @click="servicesModal.open('StableOGCTab')">
       OGC APIs
     </c-button>
   </div>
+  <p>
+    For details and help on these services,
+    please refer to Paituli
+    <c-link href='/webservices' target="_blank">
+      documentation<c-icon :path="mdiOpenInNew" size="18" />
+    </c-link>
+  </p>
 
   <ServicesModal ref="servicesModal"/>
 
@@ -38,6 +35,14 @@ const servicesModal = ref()
 p {
   margin: 0;
   margin-bottom: 10px;
+}
+.buttons {
+  display: flex;
+  gap: 10px;
+  margin: 15px;
+}
+c-button {
+  flex: 1;
 }
 
 </style>
