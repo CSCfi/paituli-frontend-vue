078f601f826538cd883dc4298945c5545f61708a
ketoni <juhana.kekoni@csc.fi>
2026-01-15 20:37:33 +0200

    InfoModal: i18n

--- src/components/modals/InfoModal.vue
+++ src/components/modals/InfoModal.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { useDatasets } from '@/composables/datasets';
 import { ref } from 'vue';
+import { useI18n } from 'vue-i18n';
 
 const { currentDataset } = useDatasets()
+const { t } = useI18n()
 
 const props = defineProps<{
   desc: string, links: { title: string, url: string }[]
@@ -24,14 +26,14 @@ defineExpose({ open })
            disable-backdrop-blur>
     <c-card>
       <c-card-title>
-        Dataset description
+        {{ t("title") }}
       </c-card-title>
 
       <c-card-content>
         <strong>{{ currentDataset?.name }} - {{ currentDataset?.org }}</strong>
         <div v-html="props.desc"></div>
         <div v-if="links.length">
-          <strong>Files describing this dataset:</strong>
+          <strong>{{ t("files") }}:</strong>
           <ul class="metadata-links">
             <li v-for="link in props.links" :key="link.url">
               <a :href="link.url" target="_blank">{{ link.title }}</a>
@@ -42,10 +44,24 @@ defineExpose({ open })
 
       <c-card-actions justify="end">
         <c-button @click="showModal = false">
-          Close
+          {{ t("close") }}
         </c-button>
       </c-card-actions>
     </c-card>
   </c-modal>
 </template>
 
+<i18n>
+{
+  "en": {
+    "title": "Dataset description",
+    "files": "Files describing this dataset",
+    "close": "Close",
+  },
+  "fi": {
+    "title": "Tietoa aineistosta",
+    "files": "Tiedostot, jotka kuvaavat tätä aineistoa",
+    "close": "Sulje",
+  },
+}
+</i18n>
