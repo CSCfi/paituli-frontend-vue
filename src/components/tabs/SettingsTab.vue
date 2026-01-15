4301d0558ecc59cd2fc92386feb046494411d6c5
ketoni <juhana.kekoni@csc.fi>
2026-01-15 21:44:34 +0200

    SettingsTab: i18n
    * Bit style formatting too

--- src/components/tabs/SettingsTab.vue
+++ src/components/tabs/SettingsTab.vue
@@ -1,32 +1,61 @@
 <script setup lang="ts">
 import { useControls } from '@/composables/controls'
 import { useSources } from '@/composables/sources'
+import { useI18n } from 'vue-i18n';
 
 const { indexLayerSource, dataLayerSource } = useSources();
 const controls = useControls();
+const { t } = useI18n()
 
 </script>
 
 <template>
+  {{ t("search") }}
+  <c-switch v-model="controls.mapsheetSearch.value" v-control>
+    {{ t("alt_search") }}
+  </c-switch>
+  <p>{{ t("show") }}...</p>
   <c-switch v-model="controls.backgroundVisible.value" v-control>
-    Show background map
+    {{ t("background") }}
   </c-switch>
   <c-switch v-model="controls.muncipalitiesVisible.value" v-control>
-    Show muncipalities layer
+    {{ t("muncipalities") }}
   </c-switch>
   <c-switch v-model="controls.catchmentVisible.value" v-control>
-    Show catchment areas layer
+    {{ t("catchment") }}
   </c-switch>
   <c-switch v-model="controls.indexVisible.value" :disabled="!indexLayerSource" v-control>
-    Show index layer
+    {{ t("index") }}
   </c-switch>
   <c-switch v-model="controls.dataVisible.value" :disabled="!dataLayerSource" v-control>
-    Show data layer
-  </c-switch>
-  <c-switch v-model="controls.mapsheetSearch.value" v-control>
-    Search selects mapsheets
+    {{ t("data") }}
   </c-switch>
 </template>
+
+<i18n>
+{
+  "en": {
+    "search": "Search",
+    "alt_search": "Select mapsheets with result bounding box",
+    "show": "Show",
+    "background": "Background map",
+    "muncipalities": "Muncipalities",
+    "catchment": "Catchment areas",
+    "index": "Index layer",
+    "data": "Data layer",
+  },
+  "fi": {
+    "search": "Haku",
+    "alt_search": "Valitse karttalehtiä haun tuloksen rajoilla",
+    "show": "Näytä",
+    "background": "Taustakartta",
+    "muncipalities": "Kuntarajat",
+    "catchment": "Valuma-alueet",
+    "index": "Indeksikartta",
+    "data": "Datakerros",
+  },
+}
+</i18n>
 
 <style scoped>
 c-switch {
@@ -38,7 +67,10 @@ c-switch {
   --c-switch-slider-color-active: var(--c-secondary-500);
   --c-switch-slider-color-disabled: var(--c-primary-700);
   --c-switch-border-color-disabled: var(--c-primary-600);
-  padding-top: 15px;
+  padding-top: 5px;
   width: 100%;
+}
+p {
+  margin-bottom: 0px;
 }
 </style>
