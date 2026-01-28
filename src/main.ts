ab29bf6b7bd077efbe0d437520560f9917046901
ketoni <juhana.kekoni@csc.fi>
2026-01-29 00:29:18 +0200

    Remove VCodeBlock package
    * Also some main.ts cleanup

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import './assets/main.css'
 import { createApp, nextTick } from 'vue'
 import { defineCustomElements } from '@cscfi/csc-ui/loader'
 import { vControl } from '@cscfi/csc-ui-vue'
-import { VCodeBlock } from '@wdns/vue-code-block';
 import { useLocale } from './composables/locale';
 
 const { i18n } = useLocale();
@@ -13,11 +12,8 @@ import router from './router'
 
 const app = createApp(App)
 app.use(router)
-app.component('VCodeBlock', VCodeBlock)
-
 app.use(i18n)
 
-// Register CSC Design System components
 app.directive('control', vControl);
 
 (async () => {
