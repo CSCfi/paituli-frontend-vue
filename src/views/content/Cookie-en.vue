ca8617262984184af616deef0047e85f9ca75156
Kylli Ek <kylli.ek@csc.fi>
2026-04-24 10:39:56 +0300

    add cookie policy and updates to privacy policy

---
+++ src/views/content/Cookie-en.vue
@@ -0,0 +1,35 @@
+<template>
+<h1>Paituli cookie policy</h1>
+
+<p>
+Cookies are small text files that browsers save on visitors’ devices. Cookies are used to keep for example the language selection of a user when they navigate between pages. 
+</p>
+<p>
+Paituli website use only necessary cookies. These essential cookies ensure that the website works as intended technically. Cookies are not used for any other purposes. The website connection is always a secure connection (https).
+</p>
+<p>
+The service providers is CSC – IT Center for Science Ltd.
+</p>
+
+
+<h2>Essential cookies used on the website</h2>
+<p>
+<b>GS_FLOW_CONTROL</b>
+</p>
+<p>
+A cookie to manage request flow and ensure the stability and performance of the GeoServer APIs, inc data preview map on Download page. This cookie is temporary and is deleted when you close the Download page.
+</p>
+
+<p>
+<b>JSESSIONID</b>
+</p>
+<p>
+A cookie to ensure the proper functioning of the GeoServer APIs, inc data preview map on Download page. It maintains session continuity between requests. This cookie is temporary and is deleted when you close the Download page.
+</p>
+
+
+<p>
+Last update 15.5.2026
+</p>
+</template>
+
