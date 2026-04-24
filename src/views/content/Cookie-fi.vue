a69c354a452d18b535a54bdb2630a2c5bf303aff
Kylli Ek <kylli.ek@csc.fi>
2026-04-24 10:47:49 +0300

    add cookie policy + updates to privacy policy + remove old html files

---
+++ src/views/content/Cookie-fi.vue
@@ -0,0 +1,33 @@
+<template>
+<h1>Paituli palvelun evästeet</h1>
+
+<p>
+Evästeet ovat pieniä tekstitiedostoja, joita selain tallentaa kävijän laitteelle. Evästeiden avulla voidaan muun muassa säilyttää käyttäjän tekemä kielivalinta, kun hän siirtyy verkkosivustolla sivulta toiselle. 
+</p>
+<p>
+Paitulissä käytetään sivuston toiminnan kannalta välttämättömiä evästeitä. Välttämättömien evästeiden avulla varmistetaan verkkosivuston tekninen toimivuus. Verkkosivustolla liikennöidään aina suojatulla yhteydellä (https).
+</p>
+<p>
+Palveluntarjoaja on CSC - Tieteen tietotekniikan keskus Oy
+</p>
+
+
+<h2>Verkkosivuston välttämättömät evästeet</h2>
+<p>
+<b>GS_FLOW_CONTROL</b>
+</p>
+<p>
+Eväste, jota käytetään pyyntöjen hallintaan sekä GeoServer-rajapintojen vakauden ja suorituskyvyn varmistamiseen, mukaan lukien lataussivulla oleva aineistojen esikatselu kartta. Eväste on välttämätön palvelun teknisen toiminnan kannalta. Eväste on istuntokohtainen ja se poistetaan, kun selausistunto päättyy.
+</p>
+
+<p>
+<b>JSESSIONID</b>
+</p>
+<p>
+Eväste, jota käytetään GeoServer-rajapintojen asianmukaisen toiminnan varmistamiseen, mukaan lukien lataussivulla oleva aineistojen esikatselu kartta. Eväste mahdollistaa istunnon ylläpitämisen pyyntöjen välillä. Eväste on välttämätön palvelun teknisen toiminnan kannalta. Eväste on istuntokohtainen ja se poistetaan, kun selausistunto päättyy.
+</p>
+
+<p>
+Last update 15.5.2026
+</p>
+</template>
