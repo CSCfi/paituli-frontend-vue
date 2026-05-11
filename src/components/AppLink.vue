5bda65929d0be701509181369c83393e3edd2bf3
ketoni <juhana.kekoni@csc.fi>
2026-05-11 21:08:34 +0300

    AppLink improvements
    * Ensuring accessible color and common color with other link-like elements
    * Restructuring main.css
    * Defaulting new-tab prop to undefined
    * Adjusting open-in-new decorator size and placement

--- src/components/AppLink.vue
+++ src/components/AppLink.vue
@@ -1,7 +1,8 @@
 <!--
-Use this component for all app links instead of
-<router-link> or <c-link>, for consistency. Using the new-tab prop
-automatically decorates the link and ensures routing is respected
+Use this component for all links instead of <router-link> or <c-link>, for consistency.
+The component tries to automatically determine if the link leads to an external site and
+in such case decorates the link and ensures it opens in a new tab, all while respecting vue routing.
+You can enforce this behavior on or off using the the new-tab prop.
 -->
 <script setup lang="ts">
 import { computed } from 'vue'
@@ -10,32 +11,37 @@ import { useRouter } from 'vue-router';
 
 const router = useRouter()
 
-const props = defineProps<{
+const {
+  to,
+  newTab = undefined,
+  useRouting,
+  cButton,
+} = defineProps<{
   to: string
   newTab?: boolean
   useRouting?: boolean
   cButton?: boolean | Record<string, unknown>
 }>()
 
-const buttonMode = computed(() => props.cButton as boolean)
-const target = computed(() => props.newTab ? '_blank' : undefined)
+const buttonMode = computed(() => cButton as boolean)
+const target = computed(() => (newTab ?? isExternal.value) ? '_blank' : undefined)
 
 const isExternal = computed(() =>
-  /^https?:\/\//.test(props.to) ||
-  props.to.startsWith('mailto:') ||
-  props.to.startsWith('tel:')
+  /^https?:\/\//.test(to) ||
+  to.startsWith('mailto:') ||
+  to.startsWith('tel:')
 )
 
 // We use routing for target href unless it's deemed external,
 // or if we explicitly want to use the router
 const resolvedHref = computed(() => {
-  if (isExternal.value && !props.useRouting) return props.to
-  return router.resolve(props.to).href
+  if (isExternal.value && !useRouting) return to
+  return router.resolve(to).href
 })
 
 // Determines props for c-button mode
 const buttonProps = computed(() => {
-  const p = props.cButton
+  const p = cButton
   // If provided prop is falsy or strictly true, use default props
   if (!p || buttonMode.value == false || p === true) return {}
   return p // Otherwise we have a record which defines the props
@@ -65,7 +71,10 @@ function onClick(navigate: (e?: MouseEvent) => void, e: MouseEvent) {
     :href="resolvedHref"
     :target="target">
     <slot />
-    <c-icon v-if="newTab" :path="mdiOpenInNew" size="18" />
+    <c-icon v-if="target"
+            :path="mdiOpenInNew"
+            size="16" />
+
   </c-button>
 
   <!-- link mode -->
@@ -74,7 +83,10 @@ function onClick(navigate: (e?: MouseEvent) => void, e: MouseEvent) {
     :href="resolvedHref"
     :target="target">
     <slot />
-    <c-icon v-if="newTab" :path="mdiOpenInNew" size="18" />
+    <c-icon v-if="target"
+            class="tab-icon"
+            :path="mdiOpenInNew"
+            size="14" />
   </c-link>
 
   <!-- In-app (SPA) navigation -->
@@ -88,7 +100,11 @@ function onClick(navigate: (e?: MouseEvent) => void, e: MouseEvent) {
       :target="target"
       @click="onClick(navigate, $event)">
       <slot />
-      <c-icon v-if="newTab" :path="mdiOpenInNew" size="18" />
+      <c-icon v-if="target"
+              class="tab-icon"
+              :path="mdiOpenInNew"
+              size="14" />
+
     </c-link>
   </router-link>
 </template>
