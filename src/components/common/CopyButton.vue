a0d53760b9dccb26378619e95a60b71209ed10a7
ketoni <juhana.kekoni@csc.fi>
2026-09-10 17:09:52 +0300

    Use CopyButton in download modal(s) too

--- src/components/common/CopyButton.vue
+++ src/components/common/CopyButton.vue
@@ -1,21 +1,22 @@
 <!--
 Small ghost button that copies the given text to the clipboard.
 Use for endpoint addresses, paths and other values the user is expected to
-paste into another tool.
+paste into another tool. Pass `label` to name the value being copied,
+e.g. "Copy PID".
 -->
 <script setup lang="ts">
 import { copyToClipboard } from '@/shared/util';
 import { mdiClipboardMultipleOutline } from '@mdi/js'
 import { useI18n } from 'vue-i18n';
 
-const { text } = defineProps<{ text: string }>()
+const { text, label } = defineProps<{ text: string, label?: string }>()
 
 const { t } = useI18n()
 </script>
 
 <template>
   <c-button ghost size="small" @click="copyToClipboard(text)">
-    {{ t('copy') }}
+    {{ label ?? t('copy') }}
     <c-icon :path="mdiClipboardMultipleOutline" size="18" />
   </c-button>
 </template>
@@ -37,5 +38,8 @@ c-button {
   margin-top: 0.2em;
   margin-bottom: 0.2em;
   margin-left: 0.5em;
+}
+c-icon {
+  --c-icon-color: var(--c-primary-600);
 }
 </style>
